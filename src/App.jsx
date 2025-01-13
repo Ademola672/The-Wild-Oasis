a153037f6104c36d5118cc6a4e5976edd18509a2
coutinho <segunademola313@gmail.com>
2025-01-13 09:21:39 +0100

    Fetching Cabin Data

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+// import React from "react";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "../src/pages/Dashboard";
+import Bookings from "../src/pages/Bookings";
+import Cabins from "../src/pages/Cabins";
+import Users from "../src/pages/Users";
+import Settings from "../src/pages/Settings";
+import Account from "../src/pages/Account";
+import Login from "../src/pages/Login";
+import PageNotFound from "../src/pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
