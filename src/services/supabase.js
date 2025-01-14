b6cad81037c1de0427905468380a1c8b0a9a4e5a
coutinho <segunademola313@gmail.com>
2025-01-14 06:16:59 +0100

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://czgridbqjftajmwtormg.supabase.co";
+export const supabaseUrl = "https://czgridbqjftajmwtormg.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6Z3JpZGJxamZ0YWptd3Rvcm1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2NzE3MjAsImV4cCI6MjA1MjI0NzcyMH0.xvRIw9Gz47h06G-jnXrLXO4CcOSEk9MRD_mtph4sF2Y";
 const supabase = createClient(supabaseUrl, supabaseKey);
